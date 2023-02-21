f535457a29f0151eb747819b0be2614cd1fa7029
Leo Mendoza Jr <dev@skyturtle.io>
2023-02-20 17:12:44 -0800

    complete Part 2 API Routes Get handlers section of tutorial

---
+++ src/routes/roll/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let number: number;
+
+	async function roll() {
+		const response = await fetch('/roll');
+		number = await response.json();
+	}
+</script>
+
+<button on:click={roll}> Roll the dice</button>
+
+{#if number !== undefined}
+	<p>You rolled a {number}</p>
+{/if}
