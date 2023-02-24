1e12afa86afc6d39f846af82993e52d7d18ec7ec
Leo Mendoza Jr <dev@skyturtle.io>
2023-02-23 22:10:25 -0500

    change to dynamic private variable

--- src/routes/envvars/+page.server.ts
+++ src/routes/envvars/+page.server.ts
@@ -1,27 +1,27 @@
 import type { PageServerLoad } from "./$types";
 import { fail, redirect } from "@sveltejs/kit";
-import { PASSPHRASE } from "$env/static/private";
+import { PASSPHRASE } from "$env/dynamic/private";
 
 export const load: PageServerLoad = ({ cookies }) => {
-	if (cookies.get("allowed")) {
-		throw redirect(307, "/envvars/welcome");
-	}
+  if (cookies.get("allowed")) {
+    throw redirect(307, "/envvars/welcome");
+  }
 };
 
 export const actions = {
-	default: async ({ request, cookies }) => {
-		const data = await request.formData();
+  default: async ({ request, cookies }) => {
+    const data = await request.formData();
 
-		if (data.get("passphrase") === PASSPHRASE) {
-			cookies.set("allowed", "true", {
-				path: "/",
-			});
+    if (data.get("passphrase") === PASSPHRASE) {
+      cookies.set("allowed", "true", {
+        path: "/",
+      });
 
-			throw redirect(303, "/envvars/welcome");
-		}
+      throw redirect(303, "/envvars/welcome");
+    }
 
-		return fail(403, {
-			incorrect: true,
-		});
-	},
+    return fail(403, {
+      incorrect: true,
+    });
+  },
 };
